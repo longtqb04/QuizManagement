c8c6eed77e547f7f36a8e991e09c5c5d77503b49
longtqb04 <tranquocbaolong2004@gmail.com>
2024-11-22 21:24:15 +0700

    Add project files.

---
+++ QuizManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuizManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
